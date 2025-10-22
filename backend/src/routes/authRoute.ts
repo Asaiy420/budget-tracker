04447fe6a9003759a01005cf5a431251072dcb50
Asaiy420 <kcaryan05@gmail.com>
2025-10-22 11:24:31 +0545

    Add login route to auth routes

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/authController";
+import { registerUser, loginUser } from "../controllers/authController";
 
 
 const router =  Router();
 
 
-router.post("/register", registerUser);
+router.post("/register", registerUser)
+router.post("/login", loginUser)
 
 export default router;
