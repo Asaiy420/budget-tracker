1c46fea07ae2785f569a02c2073cf0b782d9fc20
Asaiy420 <kcaryan05@gmail.com>
2025-10-22 11:19:53 +0545

    Mount auth routes and remove root handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
-import { Request, Response } from 'express';
 import cors from 'cors';
+import authRoutes from "./routes/authRoute"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
+app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
