b4802f06bc5407289189ab13c3b5802bd9174bf7
Asaiy420 <kcaryan05@gmail.com>
2025-10-22 11:24:26 +0545

    Refactor authController to standardize quotes and improve formatting

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
+export const registerUser = async (req: Request, res: Response) => {
+  res.send('Register endpoint');
+};
 
-
-export const registerUser = async (req:Request, res:Response)=> {
-  res.send("Register endpoint")
-}
+export const loginUser = async (req: Request, res: Response) => {
+  res.send('Login endpoint');
+};
