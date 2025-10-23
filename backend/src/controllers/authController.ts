7362ebe9ccd9164d40c999b75addc62c12759e65
Asaiy420 <kcaryan05@gmail.com>
2025-10-23 10:28:40 +0545

    Add logoutUser endpoint

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -7,3 +7,8 @@ export const registerUser = async (req: Request, res: Response) => {
 export const loginUser = async (req: Request, res: Response) => {
   res.send('Login endpoint');
 };
+
+export const logoutUser = async (req: Request, res: Response) => {
+  res.send('Logout endpoint');
+};
+
